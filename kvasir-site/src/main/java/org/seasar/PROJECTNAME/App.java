eea517072ff340b3b3118995951b6de1cd2f1b5c
hanizo <hanizo@ccb6d941-20fb-0310-8948-d27bf48cceb9>
2006-09-27 16:53:55 +0000

    initial update
    git-svn-id: https://www.seasar.org/svn/sandbox/kvasir/site/trunk@785 ccb6d941-20fb-0310-8948-d27bf48cceb9

---
+++ kvasir-site/src/main/java/org/seasar/PROJECTNAME/App.java
@@ -0,0 +1,8 @@
+package org.seasar.PROJECTNAME;
+
+public class App {
+
+    String helloWorld() {
+        return "Hello, world!";
+    }
+}
---
+++ kvasir-site/src/test/java/org/seasar/PROJECTNAME/AppTest.java
@@ -0,0 +1,10 @@
+package org.seasar.PROJECTNAME;
+
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase {
+
+    public void testHelloWorld() throws Exception {
+        assertEquals("Hello, world!", new App().helloWorld());
+    }
+}
